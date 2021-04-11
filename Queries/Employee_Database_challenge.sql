4d534800773017ba1d8aecbdb3866f167c259a29
zkirsan <77764829+zkirsan@users.noreply.github.com>
2021-04-11 17:28:11 -0400

    Added Mentorship_Eligibilty and its data

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -41,3 +41,37 @@ FROM unique_titles as ut
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+-- INTO mentor_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no= de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentor_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no= de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM mentor_eligibility;
