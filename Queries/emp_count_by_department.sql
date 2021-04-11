7e73025058274771fb8678ccf5de1422e7b744f1
zkirsan <77764829+zkirsan@users.noreply.github.com>
2021-04-11 14:14:14 -0400

    Data and the schema files

---
+++ Queries/emp_count_by_department.sql
@@ -0,0 +1,109 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+			dept_no VARCHAR(4) NOT NULL,
+			dept_name VARCHAR(40) NOT NULL,
+			PRIMARY KEY (dept_no),
+			UNIQUE (dept_name)
+			);
+			
+CREATE TABLE employees (
+			emp_no INT NOT NULL,
+			birth_date DATE NOT NULL,
+			first_name VARCHAR NOT NULL,
+			last_name VARCHAR NOT NULL,
+			gender VARCHAR NOT NULL,
+			hire_date DATE NOT NULL,
+			PRIMARY KEY (emp_no)
+);
+	
+CREATE TABLE salaries (
+ 			 emp_no INT NOT NULL,
+		     salary INT NOT NULL,
+             from_date DATE NOT NULL,
+             to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+ 			 dept_no VARCHAR NOT NULL,
+		     emp_no INT NOT NULL,
+             from_date DATE NOT NULL,
+             to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE dept_emp (
+ 		     emp_no INT NOT NULL,
+			 dept_no VARCHAR NOT NULL,
+             from_date DATE NOT NULL,
+             to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE titles (
+ 		     emp_no INT NOT NULL,
+			 title VARCHAR NOT NULL,
+             from_date DATE NOT NULL,
+             to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
