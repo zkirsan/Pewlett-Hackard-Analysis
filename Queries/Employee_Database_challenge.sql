d3aaaaa9b09fe43a757c477a2dda35eedd3d2880
zkirsan <77764829+zkirsan@users.noreply.github.com>
2021-04-11 16:56:34 -0400

    Added updated the challenge query file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,43 @@
-Employee_Database_challenge
+--Deliverable 1: The Number of Retiring Employees by Title
+
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Retrieve the created retirement_titles file
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no, to_date DESC;
+
+-- Retrieve the created unique_titles file
+SELECT * FROM unique_titles;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire
+SELECT COUNT(ut.emp_no), ut.title
+--INTO retiring titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
